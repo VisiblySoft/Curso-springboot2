dc814202ec268abd1bc521f2a86ab63f56d34dfb
visibly <adm@visibly.com.br>
2019-10-09 06:09:48 -0400

    Order

---
+++ src/main/java/com/visibly/curso/services/OrderService.java
@@ -0,0 +1,30 @@
+package com.visibly.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.visibly.curso.entities.Order;
+import com.visibly.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		
+		return repository.findAll();	
+	}
+	
+    public Order findById(Long id) {
+		
+    	Optional<Order> obj = repository.findById(id);
+		return obj.get();	
+	}
+	
+	
+}
